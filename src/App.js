32490a63ba8d343f601d503220c95296db007f08
syed basha <syedbasha94684@gmail.com>
2024-04-24 11:45:02 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react-dom'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+function App(){
+  return(
+    <div className='container'>
+      <div>
+      <h1>Super Over League</h1>
+      </div>
+      <div className='card'>
+        <img src="https://assets.ccbp.in/frontend/react-js/rcb-img.png" alt="rcb"/>
+        <img src="https://assets.ccbp.in/frontend/react-js/csk-img.png" alt="csk" />
+      </div>
     </div>
-  );
+  )
 }
-
-export default App;
+export default App
